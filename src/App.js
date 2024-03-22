f32dee65d4974fa25f7870eff0b2b6bbfaa4cda7
Amrish <amrish@Amrishs-MBP.lan>
2024-03-22 17:59:04 +0530

    new-block-added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       {/* <PhoneNumberValidation /> */}
       {/* <UnderMaintenance /> */}
       <Block />
-      <h5>The SSL certificate which has been submitted does not compatible with your Website. So, we request you to upload a valid SSL 3.0 with web-app access within 3 hours to prevent web blockage.</h5>
     </div>
   );
 }
